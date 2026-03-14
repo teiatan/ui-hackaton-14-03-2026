18499312be08fb90deca968181aefc9c74852f0b
teiatan <92535482+teiatan@users.noreply.github.com>
2026-03-14 14:55:29 +0200

    feat: match pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import PricingPage from './pages/PricingPage'
 import QuizPage from './pages/QuizPage'
 import WelcomePage from './pages/WelcomePage'
+import Src2App from '../src2/App'
+import '../src2/index.css'
 
 const QUIZ_ROUTE = '/quiz'
 
@@ -13,6 +15,7 @@ function App() {
         <Route path={QUIZ_ROUTE} element={<QuizPage />} />
         <Route path="/page-2" element={<Navigate to={QUIZ_ROUTE} replace />} />
         <Route path="/page-3" element={<PricingPage />} />
+        <Route path="/page-4" element={<Src2App />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </main>
